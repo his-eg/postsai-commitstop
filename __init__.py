6b647b7d7c3fb13df4890f5f313b19352b436659
Hendrik Brummermann <brummermann@his.de>
2017-03-07 17:07:52 +0100

    simplified setup instructions

--- __init__.py
+++ __init__.py
@@ -20,9 +20,14 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+import sys
 from backend.db import PostsaiDB
-import config
 
+try:
+    import config
+    import warnings
+except ImportError:
+    pass
 
 
 class Extension:
@@ -30,40 +35,33 @@ class Extension:
     def install_extension_setup(self, config):
         """install.py - hook invoked after reading the configuration file"""
 
-        print("Initializing " + __name__)
+        if not "repository_status_permission" in config:
+            print("ERROR: Configuration for committstop is missing.")
+            self.install_print_config_stub()
+            sys.exit(1)
+
 
 
     def install_print_config_stub(self):
         """adds stubs for retrieving the current user name and for permission checking to the config file"""
 
         print("# configuration for " + __name__)
-        print()
+        print("")
         print("""
-# checks the permission to submit a new commit stop configuration
-# (edit according to your needs)
 def repository_status_permission():
+    \"\"\"checks the permission to submit a new commit stop configuration\"\"\"
+
+    # return os.environ.get("AUTHENTICATE_POSTSAI_COMMITSTOP_MANAGER", "False") == "True"
+    # return os.environ.get("REMOTE_USER", "-") in ("admin1", "admin2")
     return True
-
-# returns the name of the user who is committing a new configuration
-# (edit according to your needs)
-def repository_status_username():
-    return "anonymous"
 """)
-
-
-    def install_pre_database_structure_update(self):
-        """install.py - hook invoked before the database structure is created or updated. 
-        Obsolete indexes and views my be deleted here"""
-
-        print("Pre database update cleanup for " + __name__)
 
 
     def install_post_database_structure_update(self):
         """install.py - hook invoked after the main database structure has been created or updated. 
         Extension can add additional tables here"""
 
-        """ XXX hier die Erstellung der Datenbank """
-        print("Post database update adjustments for " + __name__ + ": creating table repository_status")
+        """ Hier die Erstellung der Datenbank """
 
         create_config_table_sql = """\
 CREATE TABLE IF NOT EXISTS repository_status (
@@ -77,36 +75,9 @@ CREATE TABLE IF NOT EXISTS repository_status (
 """
         db = PostsaiDB(vars(config))
         db.connect()
-        rows = db.query(create_config_table_sql, None, cursor_type=None)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            db.query(create_config_table_sql, None, cursor_type=None)
         db.disconnect()
-        print "created table unless it already existed, sever response: " + str(rows) + "."
 
 
-
-    def install_post(self):
-        """install.py - hook invoked shortly before install.py is finished"""
-
-        print("Completed install for " + __name__)
-
-
-
-    def query_extension_setup(self, config):
-        """query-page hook invoked after reading the configuration file"""
-
-        pass
-
-
-    def query_create_query(self, postsai, form):
-        """query-page hook invoked after postsai.sql and postsai.data have been created, but before the statement
-           is executed. postsai.sql and postsai.data may be modified at this point"""
-
-        pass
-
-
-    def query_post_process_result(self, postsai, form, db, result):
-        """query-page hook invoked after the database query completed. The result object contains ui-configuration,
-        list of repositories, and the actual query results from the database. The result object may be modified here.
-        Extension specific information should be communicated to the client in the result["extension"][__name__]
-        namespace."""
-
-        result["extension"][__name__] = "loaded"
